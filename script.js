08c5ff7bff96d51b4d1bdae65cc8b12ba4cf8051
Siddharth <sids.aquarius@gmail.com>
2014-08-13 15:27:11 -0300

    collabmusic working app

--- script.js
+++ script.js
@@ -1,72 +1,65 @@
-function SoundCtrl($scope){
+function SoundCtrl($scope) {
 	$scope.sounds = [
 		{
-			label: 'Sound 1',
+			label: 'Accordian',
 			playing: false
 		}, {
-			label: 'Sound 2',
+			label: 'Agogo',
 			playing: false
 		}, {
-			label: 'Sound 3',
+			label: 'Bass',
 			playing: false
 		}, {
-			label: 'Sound 4',
+			label: 'Brass',
 			playing: false
 		}
 	];
 
-	$scope.toggleSound = function(sObj, index){
+	$scope.toggleSound = function(sObj, index) {
 		index++;
-		var sound = new Audio('sounds/' + index + '.ogg');
-		sObj.handler = function(){
-			sound.currentTime= 0;
-			sound.play();
+		console.log("toggle sound is getting called")
+		var sound;
+		sObj.playing = !sObj.playing; // toggle sound
+		sObj.loop = function() {
+			if (sObj.playing) {
+				if (window.chrome) sound.load()
+				else sound.pause()
+				sound.play();
+			}
 		};
-		sObj.stop = function(){
-			sound.removeEventListener('ended', this.handler);
-		};
-		if(!sObj.playing){
-			sObj.playing = true;
-			var obj = {};
-			obj[index] = sObj.playing;
-			$scope.abref.setData(obj, function(err, result) {
-				console.log("setting data")
-				console.log(result)
-			});
-			sound.addEventListener('ended', sObj.handler, false);
+		if(sObj.playing) {
+			sound = new Audio('sounds/' + index + '.wav');
 			sound.play();
+			sound.addEventListener('ended', sObj.loop, false);
 		} else {
-			sObj.stop();
+			if (sound) sound.removeEventListener('ended', this.loop);
 			sObj.playing = false;
-			var obj = {};
-			obj[index] = sObj.playing;
-			$scope.abref.setData(obj, function(err, result) {})
 		}
-		
 	};
 
-	angular.element(document).ready(function(){
-		var registered = Appbase.setApp("aphrodite", 1)
-		Appbase.new("Sound/random", function(err, result) {
-			if (!err) {
-				$scope.abref = result
-				$scope.abref.on("properties", function(err, abref, snapShot) {
-					var properties = snapShot.properties();
-					for(property in properties){
-						var number = parseInt(property);
-						if(number > 0 && number < 5){
-							number--;
-							$scope.sounds[number].playing = properties[property];
-							
-						}
-					}
-					console.log("listening here")
-					console.log(snapShot)
-					console.log(snapShot.properties())
+	$scope.setSound = function(sObj, index) {
+		// set sound to the Appbase datastore
+		var obj = {};
+		obj[index+1] = sObj.playing;
+		console.log("set sound is being called", sObj.playing, obj)
+		$scope.abref.setData(obj);
+	}
 
-				})
+	angular.element(document).ready(function() {
+		var registered = Appbase.credentials("aphrodite", "4d8d0072580912343cd74a09015cd217")
+		$scope.abref = Appbase.create("Sound", "jam")
+		$scope.abref.on("properties", function(err, abref, snapShot) {
+			var properties = snapShot.properties();
+			console.log(properties)
+			for(property in properties) {
+				var number = parseInt(property);
+				if(number > 0 && number < 5) {
+					number--
+					if (properties[property] !== $scope.sounds[number].playing)
+						$scope.toggleSound($scope.sounds[number], number)
+				}
 			}
+			$scope.$apply()
 		})
 	});
-
 }
